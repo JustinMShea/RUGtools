97685aefbbc5b6f96bb25609dc9ed744dcaf3e04
JustinMShea <jshea@mail.roosevelt.edu>
2018-04-18 12:33:39 -0500

    hex sticker script

---
+++ data-raw/hexSticker.R
@@ -0,0 +1,19 @@
+
+library(wooldridge)
+data("attend")
+
+# create plot 
+library(ggplot2)
+Graph <- ggplot(attend, aes(x = atndrte, y = termGPA))  
+
+# add labels, theme, and loess smoother
+Graph <- Graph + geom_point() + 
+                geom_smooth(se = FALSE, color = "purple") +
+                labs(x="Percent Meetup Attendance", 
+                     y="Final Grade in R skills") +
+                theme_void() + theme_transparent()
+Graph
+
+library(hexSticker)
+sticker(Graph, package="CRUGtools", p_size=26, s_x=1, s_y=0.80, s_width=1.3, 
+        s_height=1, filename="vignettes/images/CRUGtools.png")

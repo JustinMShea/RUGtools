fe88e7d5809060700ccca314105a2ae775fa6088
JustinMShea <jshea@mail.roosevelt.edu>
2018-04-18 22:20:14 -0500

    Much improved documentation

--- R/intro_slides.R
+++ R/intro_slides.R
@@ -1,12 +1,29 @@
 #' R User Group Introduction Slides
 #'
-#' This function creates an ioslides rmarkdown template, with default content useful
-#' for introducing \strong{Chicago R user group} meetups. Slides can be accessed 
-#' from within RStudio using the \strong{New R Markdown} dialog menu, and then 
-#' selecting \strong{From Template}. One can also use the `draft` function, 
+#' This function creates an ioslides R Markdown template, with default content 
+#' routinely used when introducing \strong{Chicago R user group} meetups. Slides
+#' are useful because they look good and you won't forget to do important things
+#' like \strong{thanking the Sponsors!} Slides can be accessed from within R Studio 
+#' using the \strong{New R Markdown} dialog menu, and then selecting \strong{From Template}. 
+#' One can also use the \code{\link{draft}} function, 
 #' exemplified in the Examples section below.
 #' 
+#' @details Default ioslides template includes placeholder slides for the following:
+#' \itemize{
+#' \item{\strong{Local R news: }}{Announcements about your local meetup or general R community.}
+#' \item{\strong{Global R news: }}{R Consortium announcements and upcoming conferences 
+#' like R/Finance and UseR!.}
+#' \item{\strong{R-Ladies: }}{Welcome your local R-Ladies chapter!}
+#' \item{\strong{Thank the Sponsors: }}{Display their logo and promotional material of preference.}
+#' \item{\strong{R Swag: }}{Giveaways of hex stickers, Raffle off books, etc.}
+#' \item{\strong{Tonights presentations: }}{Summary of presentations.}
+#' }
+#' 
+#' 
 #' @param ... Arguments passed to rmarkdown::ioslides_presentation function.
+#' 
+#' @source See R Studio's \href{https://rmarkdown.rstudio.com/developer_document_templates.html}{Document Templates}
+#'  for more information on creating custom R Markdown templates for your meetup.
 #' 
 #' @examples
 #' rmarkdown::draft(file="new_meetup.Rmd", template = "inst/rmarkdown/templates/intro_slides")

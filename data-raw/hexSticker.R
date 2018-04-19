c7def88e1a1dca9760d052e83d338e94c10f9adc
JustinMShea <jshea@mail.roosevelt.edu>
2018-04-18 19:29:58 -0500

    file cleanup

--- data-raw/hexSticker.R
+++ data-raw/hexSticker.R
@@ -1,19 +1,20 @@
 
 library(wooldridge)
+
+# Load data
 data("attend")
 
-# create plot 
+# create plot, add labels, theme, and loess smoother
 library(ggplot2)
-Graph <- ggplot(attend, aes(x = atndrte, y = termGPA))  
-
-# add labels, theme, and loess smoother
-Graph <- Graph + geom_point() + 
+Graph <- ggplot(attend, aes(x = atndrte, y = termGPA)) + 
+                geom_point() + 
                 geom_smooth(se = FALSE, color = "purple") +
                 labs(x="Percent Meetup Attendance", 
                      y="Final Grade in R skills") +
-                theme_void() + theme_transparent()
-Graph
+                theme_void() + 
+                theme_transparent()
 
+# create hex sticker
 library(hexSticker)
 sticker(Graph, package="CRUGtools", p_size=26, s_x=1, s_y=0.80, s_width=1.3, 
         s_height=1, filename="vignettes/images/CRUGtools.png")

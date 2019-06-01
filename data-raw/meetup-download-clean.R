cf948f814b29e53e0b826c0be4463715da59c4ea
Justin Shea <jshea@roosevelt.edu>
2019-06-01 15:28:46 -0500

    add script to drop redudant column

--- data-raw/meetup-download-clean.R
+++ data-raw/meetup-download-clean.R
@@ -11,6 +11,8 @@ member_list <- meetup_import()
 member_list$R.Experience <- stringi::stri_trans_general(member_list$R.Experience, "latin-ascii")
 member_list$Location <- stringi::stri_trans_general(member_list$Location, "latin-ascii")
 
+# Drop redundant column
+member_list$How.long.have.you.been.using.R..or.S..software.or.whatâ..s.your.interest.in.R. <- NULL
 
   #########################
  # write to .Rdata files #
